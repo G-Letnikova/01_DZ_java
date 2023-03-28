eabf4e6978df62056204fe6b58e972b021718efc
GLetnikova <g.letnikova@gmail.com>
2023-03-28 18:44:36 +0300

    change task3

--- task3.java
+++ task3.java
@@ -5,42 +5,45 @@ import java.util.Scanner;
 public class task3 {
     public static void main(String[] args) {
   
-
-
+        String askYN = "y";
         Scanner sc = new Scanner(System.in);
-        System.out.printf("int number: ");
-        int num1 = sc.nextInt();
-        System.out.printf("int action: ");
-        String act = sc.next();
-        System.out.printf("int number: ");
-        int num2 = sc.nextInt();
-        int result = 0;
-        switch (act) {
-            case "+":
-                result = num1 + num2;
+
+        while (askYN.equals("y")) {
+            System.out.printf("int number1: ");
+            int num1 = sc.nextInt();
+            System.out.printf("int action: ");
+            String act = sc.next();
+            System.out.printf("int number1: ");
+            int num2 = sc.nextInt();
+            int result = 0;
+            switch (act) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+
+                case "-":
+                result = num1 - num2;
                 break;
 
-            case "-":
-            result = num1 - num2;
-            break;
+                case "*":
+                result = num1 * num2;
+                break;
 
-            case "*":
-            result = num1 * num2;
-            break;
+                case "/":
+                result = num1 / num2;
+                break;
 
-            case "/":
-            result = num1 / num2;
-            break;
-
-            default:
-            System.out.println("error");
-            break;
-        }
+                default:
+                System.out.println("error");
+                break;
+            }
         System.out.println(result);
+        System.out.println("Продолжить? y/n: ");
+        askYN = sc.next();
 
-        sc.close();
+    }
 
-
+    sc.close();
 
     }
 }
